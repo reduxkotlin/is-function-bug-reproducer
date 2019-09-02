9a869f8f4c5e4c2a1836c3f379d4a9c0bee9d02e
Patrick Jackson <patrick.jackson@ticketmaster.com>
2019-09-02 19:35:22 -0400

    add thunk code and test

--- common/src/test/kotlin/TypealiasTest.kt
+++ common/src/test/kotlin/TypealiasTest.kt
@@ -1,7 +1,12 @@
 import kotlin.test.Test
 import kotlin.test.fail
 
+typealias Dispatch = (Any) -> Any
+typealias GetState = () -> Any
+typealias Thunk = (Dispatch, GetState, Any?) -> Any
+
 class FunctionCheckTest {
+
 
     @Test
     fun passIsCheck() {
@@ -9,5 +14,27 @@ class FunctionCheckTest {
         } else {
             fail("Function not recognized in `is Function<*>`")
         }
+    }
+
+    data class AppState(val string: String)
+
+    val dispatch: Dispatch = { }
+    val getState: GetState = { AppState("myState") }
+
+    fun myThunk(dispatch: Dispatch, getState: GetState, arg: Any?) {
+        println("Inside thunk")
+    }
+
+    fun test(foo: Any) {
+        if (foo is Function<*>) {
+            (foo as Thunk)(dispatch, getState, null)
+        } else {
+            fail()
+        }
+    }
+
+    @Test
+    fun typeAliasCheck() {
+        test(::myThunk)
     }
 }
